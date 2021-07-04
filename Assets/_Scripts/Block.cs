03825a3cb17eb444a7baf4834af03f7ba83e0178
lemonzninja <budninja36@gmail.com>
2021-07-04 16:02:35 -0400

    A/

--- Assets/_Scripts/Block.cs
+++ Assets/_Scripts/Block.cs
@@ -3,9 +3,11 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
+    [SerializeField] private AudioClip breakSound;
+    
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        AudioSource.PlayClipAtPoint(breakSound, new Vector3(0,0,0));
         Destroy(gameObject);
-        Debug.Log(collision.gameObject.name);
     }
 }
