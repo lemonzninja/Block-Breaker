dca36942bffe54dc74e43a556b017c3a61b4553e
lemonzninja <budninja36@gmail.com>
2021-07-05 21:25:32 -0400

    yes
    yes

--- Assets/_Scripts/Block.cs
+++ Assets/_Scripts/Block.cs
@@ -2,12 +2,32 @@ using System;
 using UnityEngine;
 
 public class Block : MonoBehaviour
-{
+{   
+    // The break Audio clip
     [SerializeField] private AudioClip breakSound;
     
-    private void OnCollisionEnter2D(Collision2D collision)
+    // The reference to the level Script
+    private Level _level;
+
+    private void Start()
     {
+        _level = FindObjectOfType<Level>();
+        _level.CountBreakableBlocks();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {   
+        DestroyBlock();
+    }
+
+    private void DestroyBlock()
+    {
+        /*
+         *  -Play a AudioSource of the block brake when the ball hits it.
+         *  -Destroy the block when the ball hits it.
+         */
         AudioSource.PlayClipAtPoint(breakSound, new Vector3(0,0,0));
         Destroy(gameObject);
+        _level.BlockDestroyed();
     }
 }
---
+++ Assets/_Scripts/Level.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+     [SerializeField] private int breakableBlock; // Serialized for debugging purposes
+
+     private SceneLoader _sceneLoader;
+
+     private void Start()
+     {
+          _sceneLoader = FindObjectOfType<SceneLoader>();
+     }
+
+     public void CountBreakableBlocks()
+     {
+          breakableBlock++;
+     }
+
+     public void BlockDestroyed()
+     {
+          breakableBlock--;
+          if (breakableBlock <= 0)
+          {
+               _sceneLoader.LoadNextScene();
+          }
+     }
+}
